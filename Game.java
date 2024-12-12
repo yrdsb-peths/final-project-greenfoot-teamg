99c3617c6b668cff9075e8304b791cd7c8b3e49c
Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
2024-12-12 13:19:42 -0500

    movable character

--- Game.java
+++ Game.java
@@ -1,11 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Game here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Game extends World
 {
 
@@ -15,7 +9,9 @@ public class Game extends World
      */
     public Game()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+
+        TestCharacter test = new TestCharacter();
+        addObject(test, 300, 350);
     }
 }
--- TestCharacter.java
+++ TestCharacter.java
@@ -1,19 +1,27 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class TestCharacter here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class TestCharacter extends Actor
 {
-    /**
-     * Act - do whatever the TestCharacter wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public TestCharacter() {
+        GreenfootImage image = new GreenfootImage("cupcake.jpg");
+        image.scale(50, 50);
+        setImage(image);
+    }
+
     public void act()
     {
-        // Add your action code here.
+        // Can move with WASD
+        if(Greenfoot.isKeyDown("W")) {
+            setLocation(getX(), getY() - 3);
+        }
+        else if(Greenfoot.isKeyDown("A")) {
+            setLocation(getX() - 3, getY());
+        }
+        else if(Greenfoot.isKeyDown("S")) {
+            setLocation(getX(), getY() + 3);
+        }
+        else if(Greenfoot.isKeyDown("D")) {
+            setLocation(getX() + 3, getY());
+        }
     }
 }
