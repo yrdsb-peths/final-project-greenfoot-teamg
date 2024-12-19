1d1053adb8dd40f1d3e1b2fa97de07ce2fae39bb
Charlie-006 <440028963@gapps.yrdsb.ca>
2024-12-19 13:08:51 -0500

    Changing Image for button
    Deleted the closingbutton class
    changed the file name for PauseScreen
    updated the pause button image on menuScreen

--- Button.java
+++ Button.java
@@ -18,7 +18,7 @@ public class Button extends Actor {
      * This method is called to set up or update the button image.
      * Subclasses can override this method to customize the button appearance.
      */
-    protected void updateButtonImage() {
+    public void updateButtonImage() {
         // Default button background
         GreenfootImage buttonImage = new GreenfootImage("buttonLong_beige.png");
         
@@ -62,5 +62,26 @@ public class Button extends Actor {
     public void setText(String newText) {
         this.text = newText;
         updateButtonImage(); // Refresh the button image with the new text
+    }
+    
+    public void changeButtonImage(String imageName) {
+        // Verify image exists or use default
+        GreenfootImage newButtonImage = new GreenfootImage(imageName);
+        
+        // Fallback to default if image is invalid
+        if (newButtonImage.getWidth() <= 0 || newButtonImage.getHeight() <= 0) {
+            newButtonImage = new GreenfootImage(200, 50);
+            newButtonImage.setColor(Color.LIGHT_GRAY);
+            newButtonImage.fill();
+        }
+        
+        // Reapply text
+        GreenfootImage textOverlay = new GreenfootImage(text, 23, Color.BLACK, new Color(0, 0, 0, 0));
+        
+        int textX = (newButtonImage.getWidth() - textOverlay.getWidth()) / 2;
+        int textY = (newButtonImage.getHeight() - textOverlay.getHeight()) / 2;
+        
+        newButtonImage.drawImage(textOverlay, textX, textY);
+        setImage(newButtonImage);
     }
 }
