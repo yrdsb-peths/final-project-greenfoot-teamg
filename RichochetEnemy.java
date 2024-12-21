4429e9ff2be8e26125a3c7438658ada1b5936ba1
Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
2024-12-21 16:51:17 -0500

    ricochet enemy

--- RichochetEnemy.java
+++ RichochetEnemy.java
@@ -1,31 +1,35 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class SeekingEnemy extends Enemy
+public class RichochetEnemy extends Enemy
 {
     SimpleTimer timer = new SimpleTimer();
 
-    public SeekingEnemy() {
-        GreenfootImage image = new GreenfootImage("EnemySpaceShip2.png");
+    public RichochetEnemy() {
+        GreenfootImage image = new GreenfootImage("EnemySpaceShip3.png");
         image.scale(75, 75);
         image.rotate(90);
         this.setImage(image);
         timer.mark();
     }
-
     public void act()
     {
         makeBullet();
     }
 
     public void makeBullet() {
-        if(timer.millisElapsed() > 1000) {
-            Bullet bullet = new EnemyBullet1();
+        if(timer.millisElapsed() > 500) {
+            Bullet bullet = new EnemyBullet2();
             Game game = (Game) getWorld();
             game.addObject(bullet, this.getX(), this.getY());
+
+            int toX = game.player.getX();
+            int toY = game.player.getY();
 
             // Bullet faces and spawns in front of ship initially
             bullet.setRotation(90);
             bullet.move(30);
+
+            bullet.turnTowards(toX, toY);
             
             timer.mark();
         }
