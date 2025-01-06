dbf477d13dc575d6f8d8b750bbbfa87bd982da82
Charlie-006 <440028963@gapps.yrdsb.ca>
2025-01-06 13:07:38 -0500

    added settings button and highscore button

--- CharacterSelection.java
+++ CharacterSelection.java
@@ -14,7 +14,7 @@ public class CharacterSelection extends World {
      * Constructor for CharacterSelection.
      * Sets up the character selection screen.
      */
-    public CharacterSelection(MenuScreen menuScreen) {
+    public CharacterSelection() {
         super(500, 700, 1); // Create a new world with specified dimensions
         GreenfootImage background = new GreenfootImage("CharacterSelection.jpg");
         background.scale(getWidth(), getHeight()); // Adjust to world size (500x700)
